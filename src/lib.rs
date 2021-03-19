f9195d0acb4712859b5c1f50a6b87f6bb1755fff
Philip Peterson <philip.c.peterson@gmail.com>
2021-03-18 19:58:24 -0700

    Comment out directives

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 
 #![deny(
     missing_docs,
-    missing_debug_implementations,
-    missing_copy_implementations,
+    // missing_debug_implementations,
+    // missing_copy_implementations,
     trivial_casts,
     trivial_numeric_casts,
     unsafe_code,
