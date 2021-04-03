cb7b87f91a00420e905f98a9a707a1b96ab0f01b
Philip Peterson <philip.c.peterson@gmail.com>
2021-04-02 18:01:22 -0700

    Move PrivateAnyRawNode into the internal_prelude, and cleanup

--- src/internal_prelude.rs
+++ src/internal_prelude.rs
@@ -1,6 +1,8 @@
 #![allow(unused_imports)]
 
 pub(crate) use crate::error::DomError;
-pub(crate) use crate::node::raw::{self as raw_node, element as raw_element, AnyRawNode};
+pub(crate) use crate::node::raw::{
+    self as raw_node, element as raw_element, private::PrivateAnyRawNode, AnyRawNode,
+};
 pub(crate) use crate::node::wrapped::{self as wrapped_node, element as wrapped_element};
 pub(crate) use crate::sandbox::Sandbox;
