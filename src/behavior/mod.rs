ba60e0cf23b8a44b99c925c34a36304a4727ead3
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-04-03 18:09:01 +0300

    Added docs to describe objective

--- src/behavior/mod.rs
+++ src/behavior/mod.rs
@@ -2,6 +2,10 @@
 //! refer extensively to classes and mixins, and because Rust does not support either one,
 //! this module provides several structures that provide the same behavior but in a Rust-
 //! friendly way (using composition instead of inheritance).
+//!
+//! Every behaviour is implemented using traits, which can be dependent on one another.
+//! Every behaviour has a **BehaviourName**Behaviour and **BehaviourName**BehaviourStorage and
+//! implementing macro.
 
 use std::sync::{Arc, RwLock, Weak};
 
