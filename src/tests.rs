a4ddf3c2b313ee07019c91eb3e5a1368276096c2
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-04-06 03:27:56 +0300

    added query_selector test

--- src/tests.rs
+++ src/tests.rs
@@ -3,7 +3,7 @@
 use std::sync::Arc;
 
 use crate::config::ScreenMetrics;
-use crate::node::element::HtmlHtmlElement;
+use crate::node::element::{HtmlBodyElement, HtmlButtonElement, HtmlHtmlElement};
 use crate::sandbox::Sandbox;
 
 use crate::node::AnyNode;
@@ -17,4 +17,26 @@ fn it_works() {
     let _text = doc.create_text_node("Hello, world!".to_string());
     doc.append_child(document_element);
     assert_eq!(doc.child_nodes().length(), 1);
+}
+
+#[test]
+fn query_selector() {
+    let sbox = Sandbox::new(Default::default());
+    let doc = sbox.clone().window().document();
+
+    let html = HtmlHtmlElement::new(Arc::downgrade(&sbox), ());
+    let body = HtmlBodyElement::new(Arc::downgrade(&sbox), ());
+
+    doc.append_child(html.clone());
+    html.append_child(body.clone());
+
+    let qbody = doc
+        .query_selector("body")
+        .unwrap()
+        .unwrap()
+        .downcast_arc()
+        .unwrap();
+
+    // query selector on nodes
+    assert!(Arc::ptr_eq(&qbody, &body));
 }
