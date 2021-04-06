ff11e33de3e220bb6953fb695d40917732959928
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-04-06 03:10:13 +0300

    Implemented query_selector traversal

--- src/node/query_selector.rs
+++ src/node/query_selector.rs
@@ -34,3 +34,26 @@ impl FromStr for Selector {
 pub fn is_selected(node: &Arc<dyn AnyNode>, selector: &Selector) -> bool {
     node.tag_name() == selector.0
 }
+
+/// Query selector function, does not check root node.
+/// Does not return element, returns node.
+pub fn query_selector(
+    root: &dyn AnyNode,
+    selector: &str,
+) -> Result<Option<Arc<dyn AnyNode>>, DomError> {
+    let selector = selector.parse::<Selector>()?;
+    Ok(query_selector_rec(root, &selector))
+}
+
+fn query_selector_rec(root: &dyn AnyNode, selector: &Selector) -> Option<Arc<dyn AnyNode>> {
+    root.get_node_behavior()
+        .static_child_nodes()
+        .into_iter()
+        .find_map(|node| {
+            if node.tag_name() == selector.0 {
+                Some(node)
+            } else {
+                query_selector_rec(&*node, selector)
+            }
+        })
+}
