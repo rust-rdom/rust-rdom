10b8f68f78ac17fa52e6c862fcd87cd5f38dccb9
Philip Peterson <philip.c.peterson@gmail.com>
2021-04-18 20:58:17 -0700

    Fix whitespace

--- packages/rdom/src/node/contents.rs
+++ packages/rdom/src/node/contents.rs
@@ -6,88 +6,88 @@ use crate::internal_prelude::*;
 macro_rules! declare_contents {
     ($($ti:expr => $name:ident),*) => {
         paste::paste! {
-        pub(crate) enum NodeType {
+            pub(crate) enum NodeType {
+                $(
+                    $name,
+                )*
+            }
+
+            impl NodeType {
+                pub(crate) fn get_node_number(&self) -> isize {
+                    match self {
+                        $(
+                            NodeType::$name => $ti,
+                        )*
+                    }
+                }
+            }
+
+            /// Node type, as defined in https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+            #[derive(Clone)]
+            pub(crate) enum NodeContentsArc {
+                $(
+                    $name(Arc<[<$name NodeStorage>]>),
+                )*
+            }
+
+            #[derive(Clone)]
+            pub(crate) enum NodeContentsWeak {
+                $(
+                    $name(Weak<[<$name NodeStorage>]>),
+                )*
+            }
+
+            impl NodeContentsArc {
+                pub(crate) fn to_node_type(&self) -> NodeType {
+                    match self {
+                        $(
+                            NodeContentsArc::$name(_) => NodeType::$name,
+                        )*
+                    }
+                }
+
+                pub(crate) fn downgrade(&self) -> NodeContentsWeak {
+                    match self {
+                        $(
+                            NodeContentsArc::$name(strong) => NodeContentsWeak::$name(Arc::downgrade(&strong)),
+                        )*
+                    }
+                }
+            }
+
+            impl NodeContentsWeak {
+                pub(crate) fn to_node_type(&self) -> NodeType {
+                    match self {
+                        $(
+                            NodeContentsWeak::$name(_) => NodeType::$name,
+                        )*
+                    }
+                }
+
+                pub(crate) fn upgrade(&self) -> Option<NodeContentsArc> {
+                    match self {
+                        $(
+                            NodeContentsWeak::$name(weak) => Some(NodeContentsArc::$name(weak.upgrade()?)),
+                        )*
+                    }
+                }
+            }
+
             $(
-                $name,
+                impl Builder<[<$name NodeStorage>]> {
+                    pub fn build(&self, storage: [<$name NodeStorage>]) -> ConcreteNodeArc<[<$name NodeStorage>]> {
+                        ConcreteNodeArc::<[<$name NodeStorage>]>::new(self.sandbox.clone(), Arc::new(storage))
+                    }
+                }
+
+                impl From<&Arc<[<$name NodeStorage>]>> for NodeContentsWeak {
+                    fn from(source: &Arc<[<$name NodeStorage>]>) -> NodeContentsWeak {
+                        NodeContentsWeak::$name(Arc::downgrade(source))
+                    }
+                }
             )*
         }
-
-        impl NodeType {
-            pub(crate) fn get_node_number(&self) -> isize {
-                match self {
-                    $(
-                        NodeType::$name => $ti,
-                    )*
-                }
-            }
-        }
-
-        /// Node type, as defined in https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
-        #[derive(Clone)]
-        pub(crate) enum NodeContentsArc {
-            $(
-                $name(Arc<[<$name NodeStorage>]>),
-            )*
-        }
-
-        #[derive(Clone)]
-        pub(crate) enum NodeContentsWeak {
-            $(
-                $name(Weak<[<$name NodeStorage>]>),
-            )*
-        }
-
-        impl NodeContentsArc {
-            pub(crate) fn to_node_type(&self) -> NodeType {
-                match self {
-                    $(
-                        NodeContentsArc::$name(_) => NodeType::$name,
-                    )*
-                }
-            }
-
-            pub(crate) fn downgrade(&self) -> NodeContentsWeak {
-                match self {
-                    $(
-                        NodeContentsArc::$name(strong) => NodeContentsWeak::$name(Arc::downgrade(&strong)),
-                    )*
-                }
-            }
-        }
-
-        impl NodeContentsWeak {
-            pub(crate) fn to_node_type(&self) -> NodeType {
-                match self {
-                    $(
-                        NodeContentsWeak::$name(_) => NodeType::$name,
-                    )*
-                }
-            }
-
-            pub(crate) fn upgrade(&self) -> Option<NodeContentsArc> {
-                match self {
-                    $(
-                        NodeContentsWeak::$name(weak) => Some(NodeContentsArc::$name(weak.upgrade()?)),
-                    )*
-                }
-            }
-        }
-
-        $(
-            impl Builder<[<$name NodeStorage>]> {
-                pub fn build(&self, storage: [<$name NodeStorage>]) -> ConcreteNodeArc<[<$name NodeStorage>]> {
-                    ConcreteNodeArc::<[<$name NodeStorage>]>::new(self.sandbox.clone(), Arc::new(storage))
-                }
-            }
-
-            impl From<&Arc<[<$name NodeStorage>]>> for NodeContentsWeak {
-                fn from(source: &Arc<[<$name NodeStorage>]>) -> NodeContentsWeak {
-                    NodeContentsWeak::$name(Arc::downgrade(source))
-                }
-            }
-        )*
-    }
-};
+    };
 }
 
 #[derive(Default, Clone)]
