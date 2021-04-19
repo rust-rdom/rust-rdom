09f0b0b9c6c7619e3630f9586a7b5b2edd7126fb
Philip Peterson <philip.c.peterson@gmail.com>
2021-04-18 19:29:02 -0700

    more indentation fix

--- packages/rdom/src/node/concrete.rs
+++ packages/rdom/src/node/concrete.rs
@@ -34,104 +34,104 @@ macro_rules! impl_concrete {
     ($($ti:expr => $name:ident),*) => {
         paste::paste! {
             $(
-            pub(crate) type [<$name Node>] = ConcreteNodeArc<[<$name NodeStorage>]>;
+                pub(crate) type [<$name Node>] = ConcreteNodeArc<[<$name NodeStorage>]>;
 
-            impl ConcreteNodeArc<[<$name NodeStorage>]> {
-                pub(crate) fn new(context: Weak<Sandbox>, contents: Arc<[<$name NodeStorage>]>) ->
-                ConcreteNodeArc<[<$name NodeStorage>]> {
-                    let common = Arc::new_cyclic(|construction_weak| NodeCommon {
-                        node_graph: NodeGraphStorage::new(AnyNodeWeak {
-                            contents: (&contents).into(),
-                            common: construction_weak.clone(),
-                        }),
-                        context,
-                    });
+                impl ConcreteNodeArc<[<$name NodeStorage>]> {
+                    pub(crate) fn new(context: Weak<Sandbox>, contents: Arc<[<$name NodeStorage>]>) ->
+                    ConcreteNodeArc<[<$name NodeStorage>]> {
+                        let common = Arc::new_cyclic(|construction_weak| NodeCommon {
+                            node_graph: NodeGraphStorage::new(AnyNodeWeak {
+                                contents: (&contents).into(),
+                                common: construction_weak.clone(),
+                            }),
+                            context,
+                        });
 
-                    ConcreteNodeArc { contents, common }
-                }
-            }
-
-            impl SandboxMemberBehavior for ConcreteNodeArc<[<$name NodeStorage>]> {
-                fn get_context(&self) -> Weak<Sandbox> {
-                    self.common.context.clone()
-                }
-            }
-
-            impl TryFrom<AnyNodeArc> for ConcreteNodeArc<[<$name NodeStorage>]> {
-                type Error = DomError;
-
-                fn try_from(value: AnyNodeArc) -> Result<Self, Self::Error> {
-                    let contents = match value.contents {
-                        NodeContentsArc::$name(element) => Ok(element),
-                        _ => Err(DomError::NodeCastFail),
-                    }?;
-
-                    Ok(ConcreteNodeArc {
-                        contents,
-                        common: value.common,
-                    })
-                }
-            }
-
-            impl TryFrom<AnyNodeWeak> for ConcreteNodeWeak<[<$name NodeStorage>]> {
-                type Error = DomError;
-
-                fn try_from(value: AnyNodeWeak) -> Result<Self, Self::Error> {
-                    let contents = match value.contents {
-                        NodeContentsWeak::$name(element) => Ok(element),
-                        _ => Err(DomError::NodeCastFail),
-                    }?;
-
-                    Ok(ConcreteNodeWeak {
-                        contents,
-                        common: value.common
-                    })
-                }
-            }
-
-            impl From<ConcreteNodeArc<[<$name NodeStorage>]>> for AnyNodeArc {
-                fn from(concrete: ConcreteNodeArc<[<$name NodeStorage>]>) -> Self {
-                    AnyNodeArc {
-                        common: concrete.common,
-                        contents: NodeContentsArc::$name(concrete.contents),
+                        ConcreteNodeArc { contents, common }
                     }
                 }
-            }
 
-            impl From<ConcreteNodeWeak<[<$name NodeStorage>]>> for AnyNodeWeak {
-                fn from(concrete: ConcreteNodeWeak<[<$name NodeStorage>]>) -> Self {
-                    AnyNodeWeak {
-                        common: concrete.common,
-                        contents: NodeContentsWeak::$name(concrete.contents),
+                impl SandboxMemberBehavior for ConcreteNodeArc<[<$name NodeStorage>]> {
+                    fn get_context(&self) -> Weak<Sandbox> {
+                        self.common.context.clone()
                     }
                 }
-            }
 
-            impl NodeBehaviour for ConcreteNodeArc<[<$name NodeStorage>]> {
-                fn first_child(&self) -> Option<AnyNodeArc> {
-                    self.common.node_graph.first_child()
+                impl TryFrom<AnyNodeArc> for ConcreteNodeArc<[<$name NodeStorage>]> {
+                    type Error = DomError;
+
+                    fn try_from(value: AnyNodeArc) -> Result<Self, Self::Error> {
+                        let contents = match value.contents {
+                            NodeContentsArc::$name(element) => Ok(element),
+                            _ => Err(DomError::NodeCastFail),
+                        }?;
+
+                        Ok(ConcreteNodeArc {
+                            contents,
+                            common: value.common,
+                        })
+                    }
                 }
 
-                fn last_child(&self) -> Option<AnyNodeArc> {
-                    self.common.node_graph.last_child()
+                impl TryFrom<AnyNodeWeak> for ConcreteNodeWeak<[<$name NodeStorage>]> {
+                    type Error = DomError;
+
+                    fn try_from(value: AnyNodeWeak) -> Result<Self, Self::Error> {
+                        let contents = match value.contents {
+                            NodeContentsWeak::$name(element) => Ok(element),
+                            _ => Err(DomError::NodeCastFail),
+                        }?;
+
+                        Ok(ConcreteNodeWeak {
+                            contents,
+                            common: value.common
+                        })
+                    }
                 }
 
-                fn append_child(&self, other: AnyNodeArc) {
-                    self.common.node_graph.append_child(other)
+                impl From<ConcreteNodeArc<[<$name NodeStorage>]>> for AnyNodeArc {
+                    fn from(concrete: ConcreteNodeArc<[<$name NodeStorage>]>) -> Self {
+                        AnyNodeArc {
+                            common: concrete.common,
+                            contents: NodeContentsArc::$name(concrete.contents),
+                        }
+                    }
                 }
 
-                fn child_nodes(&self) -> Arc<NodeList> {
-                    self.common.node_graph.child_nodes()
+                impl From<ConcreteNodeWeak<[<$name NodeStorage>]>> for AnyNodeWeak {
+                    fn from(concrete: ConcreteNodeWeak<[<$name NodeStorage>]>) -> Self {
+                        AnyNodeWeak {
+                            common: concrete.common,
+                            contents: NodeContentsWeak::$name(concrete.contents),
+                        }
+                    }
                 }
 
-                fn clone_node(&self) -> AnyNodeArc {
-                    AnyNodeArc::from(self.clone()).clone_node()
-                }
+                impl NodeBehaviour for ConcreteNodeArc<[<$name NodeStorage>]> {
+                    fn first_child(&self) -> Option<AnyNodeArc> {
+                        self.common.node_graph.first_child()
+                    }
 
-                fn get_node_type(&self) -> isize {
-                    $ti
+                    fn last_child(&self) -> Option<AnyNodeArc> {
+                        self.common.node_graph.last_child()
+                    }
+
+                    fn append_child(&self, other: AnyNodeArc) {
+                        self.common.node_graph.append_child(other)
+                    }
+
+                    fn child_nodes(&self) -> Arc<NodeList> {
+                        self.common.node_graph.child_nodes()
+                    }
+
+                    fn clone_node(&self) -> AnyNodeArc {
+                        AnyNodeArc::from(self.clone()).clone_node()
+                    }
+
+                    fn get_node_type(&self) -> isize {
+                        $ti
+                    }
                 }
-            }
             )*
         }
     }
