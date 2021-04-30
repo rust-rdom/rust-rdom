2335d13f92f232f7efb7ff632f02825ff02096db
Maxim <maxim.kosterov@gmail.com>
2021-04-30 04:56:42 +0300

    restructured build.rs a bit

--- packages/rdom/build.rs
+++ packages/rdom/build.rs
@@ -1,9 +1,5 @@
 use serde_derive::Deserialize;
-use std::{
-    collections::HashMap,
-    fmt::format,
-    fs::{self, read_to_string},
-};
+use std::{collections::HashMap, fs::read_to_string};
 
 use proc_macro2::TokenStream;
 use quote::quote;
@@ -13,7 +9,7 @@ macro_rules! declare_generators {
     ($($g:literal),*) => {
         &[
             $(
-                ($g, &Generator($g) as &dyn SourceGenerator),
+                ($g, &Generator::load($g) as &dyn SourceGenerator),
             )*
         ]
     };
@@ -27,24 +23,29 @@ fn main() {
     run_sourcegen(&parameters).unwrap();
 }
 
-fn load_yaml(name: &str) -> String {
-    read_to_string(format!("generators/{}.yaml", name)).unwrap()
-}
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Template {
     fields: HashMap<String, String>,
 }
 
 #[derive(Debug)]
-struct Generator<'s>(pub &'s str);
+struct Generator(pub Template);
 
-impl SourceGenerator for Generator<'_> {
+impl Generator {
+    fn load(path: &str) -> Generator {
+        let contents = read_to_string(format!("generators/{}.yaml", path)).unwrap();
+        Generator(serde_yaml::from_str(&contents).unwrap())
+    }
+}
+
+impl SourceGenerator for Generator {
     fn generate_struct(
         &self,
         _args: syn::AttributeArgs,
         item: &syn::ItemStruct,
     ) -> Result<Option<TokenStream>, anyhow::Error> {
+        let template = &self.0;
+
         let ident = &item.ident;
         let vis = &item.vis;
 
@@ -59,18 +60,15 @@ impl SourceGenerator for Generator<'_> {
 
         let generics = &item.generics;
 
-        let generator: Template = serde_yaml::from_str(&load_yaml(self.0)).unwrap();
-
         let definition = {
-            let fields =
-                generator
-                    .fields
-                    .iter()
-                    .fold(TokenStream::new(), |mut acc, (ident, ty)| {
-                        let stream: TokenStream = format!("{}: {},", ident, ty).parse().unwrap();
-                        acc.extend(stream);
-                        acc
-                    });
+            let fields = template
+                .fields
+                .iter()
+                .fold(TokenStream::new(), |mut acc, (ident, ty)| {
+                    let stream: TokenStream = format!("{}: {},", ident, ty).parse().unwrap();
+                    acc.extend(stream);
+                    acc
+                });
 
             quote! {
                 #attrs
