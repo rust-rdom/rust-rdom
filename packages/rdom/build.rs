5fe12d42f19dba6dfd676e66f869da4e3b7796c5
Maxim <maxim.kosterov@gmail.com>
2021-04-30 21:38:45 +0300

    Added automatic generator files detection

--- packages/rdom/build.rs
+++ packages/rdom/build.rs
@@ -1,23 +1,49 @@
 use serde_derive::Deserialize;
-use std::{collections::HashMap, fs::read_to_string};
+use std::{
+    collections::HashMap,
+    fs::{self, read_to_string},
+    path::Path,
+};
 
 use proc_macro2::TokenStream;
 use quote::quote;
 use sourcegen_cli::{run_sourcegen, SourceGenerator, SourcegenParameters};
 
-macro_rules! declare_generators {
-    ($($g:literal),*) => {
-        &[
-            $(
-                ($g, &Generator::load($g) as &dyn SourceGenerator),
-            )*
-        ]
-    };
+// recursive function traverses `dir`
+// it also adds prefixes with '-' if the file is in a folder
+fn read_dir(dir: impl AsRef<Path>, prefix: String, collect: &mut Vec<(String, Generator)>) {
+    for entry in fs::read_dir(dir).unwrap().map(Result::unwrap) {
+        // srry, this code is kind of a mess
+        //                        - maksimil
+        if entry.file_type().unwrap().is_file() {
+            let key = format!("{}{}", prefix, entry.file_name().to_str().unwrap());
+            let key = key[0..key.len() - 5].to_string();
+            let generator = Generator::load(&entry.path());
+            collect.push((key, generator));
+        } else if entry.file_type().unwrap().is_dir() {
+            let prefix = format!("{}{}-", prefix, entry.file_name().to_str().unwrap());
+            read_dir(&entry.path(), prefix, collect);
+        }
+    }
 }
 
 fn main() {
+    // data storage
+    let data = {
+        let mut data = Vec::new();
+        read_dir("generators", "".to_string(), &mut data);
+        data
+    };
+
+    // storage for references to that data (because sourcegen is built this way)
+    let dataref = data
+        .iter()
+        .map(|(s, g)| (s.as_str(), g as &dyn SourceGenerator))
+        .collect::<Vec<_>>();
+
+    eprintln!("{:?}", data);
     let parameters = SourcegenParameters {
-        generators: declare_generators!("window"),
+        generators: &dataref[..],
         ..Default::default()
     };
     run_sourcegen(&parameters).unwrap();
@@ -32,8 +58,9 @@ struct Template {
 struct Generator(pub Template);
 
 impl Generator {
-    fn load(path: &str) -> Generator {
-        let contents = read_to_string(format!("generators/{}.yaml", path)).unwrap();
+    fn load(path: impl AsRef<Path>) -> Generator {
+        eprintln!("{:?}", path.as_ref());
+        let contents = read_to_string(path).unwrap();
         Generator(serde_yaml::from_str(&contents).unwrap())
     }
 }
