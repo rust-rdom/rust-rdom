6d95de211f5d190883bed3c76a82dc50426c4bd6
Philip Peterson <philip.c.peterson@gmail.com>
2021-05-01 23:41:18 -0700

    wip

--- ci-scripts/check-no-sourcegen-changes.sh
+++ ci-scripts/check-no-sourcegen-changes.sh
@@ -5,20 +5,14 @@ set -e
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-
-echo 'AAAAA'
-git diff --exit-code
-echo 'BBBBB'
-git diff --cached  --exit-code
-
-if ( (git diff --exit-code || git diff --cached --exit-code) 2>&1 > /dev/null ); then
+if ( (!(git diff --exit-code) || !(git diff --cached --exit-code)) 2>&1 > /dev/null ); then
 	echo 'Cannot run script; working directory has been modified'
 	exit 1
 fi
 
-"$SCRIPTPATH"/sourcegen.sh
+"$SCRIPTPATH"/../sourcegen.sh
 
-if ( (git diff --exit-code || git diff --cached --exit-code) 2>&1 > /dev/null ); then
+if ( (!(git diff --exit-code) || !(git diff --cached --exit-code)) 2>&1 > /dev/null ); then
 	echo 'Uncommitted code generation changes detected. Run sourcegen.sh and commit the result to fix.'
 	exit 2
 fi
