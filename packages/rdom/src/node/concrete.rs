11aefdd040e9a7270226c5c16ce4417564fb0ee7
Philip Peterson <philip-peterson@users.noreply.github.com>
2021-09-17 17:52:36 -0700

    Support for attributes (#53)
    * Simple fixup
    
    * WIP Element methods
    
    * Support for getting/setting element attributes

--- packages/rdom/src/node/concrete.rs
+++ packages/rdom/src/node/concrete.rs
@@ -1,8 +1,10 @@
 //! Concrete (as opposed to abstract) types of nodes. Each node class is represented in this module.
 
 use crate::internal_prelude::*;
+use crate::node::element::ElementStore;
+use crate::node_list::NodeList;
 use crate::selector::Selector;
-use crate::{proxy_node_behavior, proxy_parent_node_behavior};
+use crate::{impl_parent_node, proxy_node_behavior, proxy_parent_node_behavior};
 
 use super::contents::{
     AttributeStore, CDataSectionStore, CommentStore, DocumentFragmentStore, DocumentStore,
@@ -12,10 +14,10 @@ use super::{
     AnyNodeStore, Buildable, NodeBehavior, NodeCommon, NodeContentsArc, NodeContentsWeak,
     NodeGraphStorage,
 };
-use crate::node::element::ElementStore;
-use crate::node_list::NodeList;
+
 use arc_new_cyclic_n::arc::new_cyclic_2;
 use std::convert::TryFrom;
+
 crate::use_behaviors!(parent_node);
 
 /// A strongly-typed handle to a node with a strong reference.
@@ -233,9 +235,7 @@ impl ConcreteNodeArc<ElementStore> {
 
 impl ConcreteNodeArc<DocumentStore> {
     proxy_parent_node_behavior!();
-}
 
-impl DocumentNodeArc {
     /// Creates a new text node with the given text contents
     pub fn create_text_node(&self, text: String) -> Result<TextNodeArc, DomError> {
         match self.get_context().upgrade() {
